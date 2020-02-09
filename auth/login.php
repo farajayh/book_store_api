de7e52ce596cd9544620e53e49576c93c22f0c2f
Ifara Joshua <farajayh@gmail.com>
2020-02-09 07:35:33 +0100

    Added jwt generation

--- auth/login.php
+++ auth/login.php
@@ -7,10 +7,14 @@
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
     // get database connection
-    include_once '../config/db_connection.php';
+    include_once "../config/db_connection.php";
     
+    //include JWT library
+    require_once "../vendor/autoload.php";
+    use Firebase\JWT\JWT;
+
     // instantiate book object
-    include_once '../objects/users.php';
+    include_once "../objects/users.php";
 
     require_once "../auth/Bcrypt.php";
     $bc = new Bcrypt();
@@ -35,9 +39,37 @@
         if($bc->check_password($password, $password_hash)){
             // set response code - 201 created
             http_response_code(201);
-    
-            // tell the user
-            echo json_encode(array("message" => "Login successful."));
+            
+            //generate token
+            $secret_key = "$2a$08l9bw8Noni4Bd/ka9RJYK0u3cSOCIC81YgJs";
+            $issuer_claim = "localhost_server";
+            $audience_claim = "book_client";
+            $issued_at_claim = time();
+            $not_before_claim = time()+10;
+            $expire_claim = time()+60;
+
+            $token = array(
+                "iss" => $issuer_claim,
+                "aud" => $audience_claim,
+                "iat" => $issued_at_claim,
+                "nbf" => $not_before_claim,
+                "exp" => $expire_claim,
+                "data" => array (
+                "id" => $result['id'],
+                "first_name" => $result['first_name'],
+                "last_name" => $result['last_name'],
+                "email" => $result['email']
+            ));
+
+            $jwt = JWT::encode($token, $secret_key);
+            
+            //show output to the user
+            echo json_encode(
+                array(
+                    "message" => "Login successful.",
+                    "token" => $jwt,
+                    "expires_at" => date(" d/m/y G:i:s", $expire_claim)
+            ));
         }else{
             // set response code - 503 service unavailable
             http_response_code(503);
